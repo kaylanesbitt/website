57b54f09deababb622c96eed03bc3d38575f42a1
Kayla Nesbitt <77808103+kaylanesbitt@users.noreply.github.com>
2023-04-13 14:39:21 -0400

    working on activities

--- src/routes/Activities.js
+++ src/routes/Activities.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import './Activities.css'
+import gwc from '../assets/gwc_logo.png'
+import unc from '../assets/unc_logo.png'
+import techx from '../assets/techx_icon.jpeg'
 
 export const Activities = () => {
     return (
         <div>
-            <p>hi mom</p>
-        </div>
+            <div>
+                <Navbar></Navbar>
+            </div>
+            <div>
+                <article className='techx'>
+                    <h4 >TechX - Cohort President</h4>
+
+                    <img className='techx-image' src={techx} alt=''></img>
+                    <p>TechX is a society designed to develop undergraduates' technical skills. During my membership in the club, I have
+                        learned how to make front-end websites using React, and how to create a database system and backend server using Node.
+                        As cohort president, I act as a spokesperson for Cohort I during exec board meetings, and a medium through which exec
+                        can make announcements. My industry experience also makes me a great resource for younger members in the club, and this week,
+                        I will present on how to get an internship for summer 2024.
+                        This is my first semester as a member of TechX.</p>
+                </article>
+                <article className='pickle-club'>
+                    <h4>UNC Club Pickleball - Member</h4>
+                    <p>
+                        As a member of UNC Club Pickleball, I have participated in about 3 collegiate matches and 2 collegiate tournaments.
+                        This club has been a great way for me to meet a wide range of people on UNC campus, including graduate students!
+                        I have been a member of UNC club pickleball for 3 semesters.
+                    </p>
+                </article>
+                <article className='gwc'>
+                    <h4>Girls Who Code - Teaching Assistant</h4>
+                    <p>
+                        Girls Who Code is a club that provides classes to local middle and high school girls to introduce them to computer science topics.
+                        As a teaching assistant, I attended class every other week and helped students work on their semester-long projects.
+                        I volunteered as a TA for two semesters.
+                    </p>
+                </article>
+            </div >
+        </div >
     )
 }
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -9,8 +9,20 @@ export const Projects = () => {
                 <Navbar></Navbar>
             </div>
             <div>
-                <h4>My personal website</h4>
-                <p>You're looking at this project right now! Click this <a href='https://www.linkedin.com/in/kayla-nesbitt/' target='_blank'></a></p>
+                <article className='website'>
+                    <h4>My personal website</h4>
+                    <p>You're looking at this project right now! Click this <a href='https://github.com/kaylanesbitt/website/tree/main' target='_blank'>link</a> to visit the GitHub repo.</p>
+                </article>
+                <article className='prof-org'>
+                    <h4>A Project for the Betterment of Students' Mental Health</h4>
+                    <p>If you are or have been an undergraduate at an American university, you have probably experienced the phenomenon that
+                        is colloquially referred to as a 'hell week.' This is when you have multiple tests and/or projects due in one week.</p>
+                    <p>I cannot claim to know why this always happens, but my team in TechX wanted to come up with a way for professors to see
+                        the exam schedules of their students. The idea is for each class, there is a calendar that displays the exam dates of all the classes
+                        that the students are taking, with the number of students who have to take that test.
+                    </p>
+                    <p>It is currently in progress. Click this <a href='https://github.com/kaylanesbitt/anti-hell-week' target='_blank'>link</a> to visit the GitHub repo.</p>
+                </article>
             </div>
         </div>
     )
